e5bc388b61f519d657b442a675c87abfe9f37910
joshuanatanielnm <clsjoshua@gmail.com>
2022-07-25 23:50:10 +0700

    feat: create on boarding page

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,21 +1,54 @@
-import { IonSlides, IonSlide, IonContent } from "@ionic/react";
+import { IonSlides, IonSlide, IonContent, IonButton } from "@ionic/react";
+import "./Onboarding.scss";
 
 const Onboarding = () => {
   const slideOpts = {
-    initialSlide: 1,
     speed: 400,
   };
   return (
-    <IonContent>
-      <IonSlides pager={true} options={slideOpts}>
+    <IonContent fullscreen>
+      <IonSlides pager={true} options={slideOpts} className="ion">
         <IonSlide>
-          <h1>Slide 1</h1>
+          <div className="image-container">
+            <img src="/assets/ilu1.png" alt="ilu1" />
+          </div>
+          <div className="slider-text">
+            <h2>Temukan Kebutuhan Panganmu</h2>
+
+            <p>
+              Telusuri harta karun yang telah orang lain berikan kepada dunia
+              untuk kebutuhan panganmu
+            </p>
+          </div>
+          <IonButton>Lewati</IonButton>
         </IonSlide>
         <IonSlide>
-          <h1>Slide 2</h1>
+          <div className="image-container">
+            <img src="/assets/ilu2.png" alt="ilu1" />
+          </div>
+          <div className="slider-text">
+            <h2>Jalin Sosialisasi untuk Pangan Lebih Baik</h2>
+
+            <p>
+              Temukan cara baru untuk saling terkoneksi satu sama lain dengan
+              cara berbagi makanan
+            </p>
+          </div>
+          <IonButton>Lewati</IonButton>
         </IonSlide>
         <IonSlide>
-          <h1>Slide 3</h1>
+          <div className="image-container">
+            <img src="/assets/ilu3.png" alt="ilu1" />
+          </div>
+          <div className="slider-text">
+            <h2>Bagikan Makananmu Untuk Kebaikan</h2>
+
+            <p>
+              Bergabunglah bersama kami untuk dapat berbagi kebaikan kepada
+              dunia melalui pangan
+            </p>
+          </div>
+          <IonButton>Mulai</IonButton>
         </IonSlide>
       </IonSlides>
     </IonContent>
