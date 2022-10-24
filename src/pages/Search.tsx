d4d5937c8e2b175b2f1060a9f82669c441f8c146
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-24 18:13:01 +0700

    feat: implement search function on search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -16,6 +16,7 @@ import { arrowBack, filterCircleOutline } from "ionicons/icons";
 import { useEffect, useRef, useState } from "react";
 import { OverlayEventDetail } from "@ionic/core/components";
 import { ContentCard } from "../components/ContentCard";
+import axios from "axios";
 
 type KotaProps = {
   provinsi: string;
@@ -28,11 +29,29 @@ export const Search = () => {
   const [filter, setFilter] = useState<any>();
   const [isLoading, setIsLoading] = useState(true);
   const [kota, setKota] = useState([]);
+  const [searchKota, setSearchKota] = useState("");
+  const [searchData, setSearchData] = useState("");
+  const [selectedKota, setSelectedKota] = useState("");
+  const [data, setData] = useState<any>([]);
+  const filteredKota = kota
+    .filter((v: KotaProps) => v.provinsi.toLowerCase() === "jawa timur")
+    .map((v: KotaProps) => v.kota)
+    .flat(1)
+    .filter((v) => v.toLowerCase().includes(searchKota.toLowerCase()))
+    .map((v) => v);
+
+  const filteredData = data.filter(
+    (v: any) =>
+      v.postingLocation.includes(selectedKota) &&
+      v.postingDescription.toLowerCase().includes(searchData.toLowerCase())
+  );
+  console.log(filteredData);
 
   const fetchDataKota = async () => {
     const data = await fetch(
       "https://raw.githubusercontent.com/mtegarsantosa/json-nama-daerah-indonesia/master/regions.json"
     );
+
     return data.json();
   };
 
@@ -42,6 +61,13 @@ export const Search = () => {
       .then((value) => setKota(value))
       .catch((error) => console.error(error))
       .finally(() => setIsLoading(false));
+
+    async function fetchData() {
+      axios.get("http://localhost:8080/posting").then((response) => {
+        setData(response.data["posting"]);
+      });
+    }
+    fetchData();
   }, []);
 
   function confirm(kota: string) {
@@ -58,40 +84,63 @@ export const Search = () => {
       <Content>
         <IonContent fullscreen className="ion-padding">
           <IonRow className="ion-justify-content-between ion-align-items-center">
-            <IonCol size="10">
-              <IonSearchbar placeholder="Mau makan apa hari ini?" />
+            <IonCol size="9">
+              <IonSearchbar
+                placeholder="Mau makan apa hari ini?"
+                onIonChange={(e) => setSearchData(e.target.value!)}
+                value={searchData}
+              />
             </IonCol>
-            <IonCol size="2">
-              <IonButton fill="clear" id="open-modal">
-                <IonIcon icon={filterCircleOutline} size="large" color="gray" />
-              </IonButton>
+            <IonCol size="3">
+              {selectedKota ? (
+                <IonButton
+                  fill="solid"
+                  id="open-modal"
+                  style={{ fontSize: "10px" }}
+                >
+                  {selectedKota}
+                </IonButton>
+              ) : (
+                <IonButton fill="clear" id="open-modal">
+                  <IonIcon
+                    icon={filterCircleOutline}
+                    size="large"
+                    color="gray"
+                  />
+                </IonButton>
+              )}
             </IonCol>
           </IonRow>
+
           <div style={{ padding: 3 }}>
-            <ContentCard
-              userName="Joshua Suherman"
-              userCity="Tangerang"
-              desc="Beberapa stok sayuran yang akan dibuang jika tidak ada pembeli
-                  di Supermarket Indo..."
-              img="/assets/food/vegetable.png"
-              numberOfComment="12"
-            />
-            <ContentCard
-              userName="Joshua Suherman"
-              userCity="Tangerang"
-              desc="Beberapa stok sayuran yang akan dibuang jika tidak ada pembeli
-                  di Supermarket Indo..."
-              img="/assets/food/vegetable.png"
-              numberOfComment="12"
-            />
-            <ContentCard
-              userName="Joshua Suherman"
-              userCity="Tangerang"
-              desc="Beberapa stok sayuran yang akan dibuang jika tidak ada pembeli
-                  di Supermarket Indo..."
-              img="/assets/food/vegetable.png"
-              numberOfComment="12"
-            />
+            {/* <div style={{ marginBottom: 20 }}>Kota: all</div> */}
+
+            {filteredData.length > 0 ? (
+              filteredData.map((v: any) => (
+                <ContentCard
+                  key={v.id}
+                  userName={v.User.userProfileName}
+                  userCity={v.postingLocation}
+                  desc={v.postingDescription}
+                  img={v.postingImage}
+                  numberOfComment={v.Comment.length}
+                />
+              ))
+            ) : (
+              <IonCol>
+                <div className="image-container">
+                  <img src="/assets/searchNotFound.png" alt="ilu1" />
+                </div>
+                <div style={{ textAlign: "center" }}>
+                  <h2>Pencarian Tidak Ditemukan</h2>
+
+                  <p>
+                    Coba cari kata lain atau ubah filter untuk menemukan apa
+                    yang kamu cari
+                  </p>
+                </div>
+              </IonCol>
+            )}
           </div>
 
           <IonModal
@@ -107,20 +156,28 @@ export const Search = () => {
                 />
               </IonCol>
               <IonCol size="10">
-                <IonSearchbar placeholder="Pilih Kota" />
+                <IonSearchbar
+                  placeholder="Pilih Kota"
+                  onIonChange={(e) => setSearchKota(e.target.value!)}
+                  value={searchKota}
+                />
               </IonCol>
             </IonRow>
             <IonContent>
               <IonList>
                 {!isLoading &&
-                  kota &&
-                  kota.map((v: KotaProps) => {
-                    return v.kota.map((kota) => (
-                      <IonItem onClick={() => confirm(kota)} key={kota}>
-                        <IonLabel ref={item}>{kota}</IonLabel>
-                      </IonItem>
-                    ));
-                  })}
+                  filteredKota &&
+                  filteredKota.map((kota) => (
+                    <IonItem
+                      onClick={() => {
+                        confirm(kota);
+                        setSelectedKota(kota);
+                      }}
+                      key={kota}
+                    >
+                      <IonLabel ref={item}>{kota}</IonLabel>
+                    </IonItem>
+                  ))}
               </IonList>
             </IonContent>
           </IonModal>
