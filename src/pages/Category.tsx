e7cd03ad8478ba31ff7d4672e71b45281b5e6620
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-24 18:13:51 +0700

    feat: update category pages if there's no data

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -6,7 +6,13 @@ import {
   IonButtons,
   IonBackButton,
   IonTitle,
+  IonContent,
+  IonCol,
 } from "@ionic/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Content } from "../layout/Content";
+import { ContentCard } from "../components/ContentCard";
 
 export const Category = ({ match }: any) => {
   const {
@@ -18,6 +24,21 @@ export const Category = ({ match }: any) => {
       letter.toUpperCase()
     );
   }
+  const [data, setData] = useState<any>([]);
+  const [error, setError] = useState<any>("");
+  const filteredData = data.filter(
+    (v: any) => v.postingCategory.toLowerCase() === category
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      axios.get("http://localhost:8080/posting").then((response) => {
+        setData(response.data["posting"]);
+      });
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <IonPage>
@@ -31,6 +52,39 @@ export const Category = ({ match }: any) => {
           </IonTitle>
         </IonToolbar>
       </IonHeader>
+
+      <Content>
+        <IonContent fullscreen className="ion-padding">
+          <div style={{ padding: 3 }}>
+            {filteredData.length > 0 ? (
+              filteredData.map((v: any) => (
+                <ContentCard
+                  key={v.id}
+                  userName={v.User.userProfileName}
+                  userCity={v.postingLocation}
+                  desc={v.postingDescription}
+                  img={v.postingImage}
+                  numberOfComment={v.Comment.length}
+                />
+              ))
+            ) : (
+              <IonCol>
+                <div className="image-container">
+                  <img src="/assets/searchNotFound.png" alt="ilu1" />
+                </div>
+                <div style={{ textAlign: "center" }}>
+                  <h2>Pencarian Tidak Ditemukan</h2>
+
+                  <p>
+                    Coba cari kata lain atau ubah filter untuk menemukan apa
+                    yang kamu cari
+                  </p>
+                </div>
+              </IonCol>
+            )}
+          </div>
+        </IonContent>
+      </Content>
     </IonPage>
   );
 };
